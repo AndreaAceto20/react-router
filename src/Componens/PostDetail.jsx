f5ac1d37ccdeac6a93c70ca43c27c3cb8e112922
Andrea Aceto <andrea.aceto20@gmail.com>
2025-03-03 17:52:14 +0100

    [ADD]Detail page

--- src/Componens/PostDetail.jsx
+++ src/Componens/PostDetail.jsx
@@ -8,5 +8,26 @@ import { useParams } from "react-router-dom"
 
 
 export default function PostDetail() {
+    const { id } = useParams();
+
+    const [post, setPost] = useState({})
+
+    function fetchPost() {
+        axios.get(`http://localhost:3000/posts/${id}`)
+            .then(res => setPost(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(
+        () => fetchPost(),
+        [id])
+
+
+    return (
+        <>
+            <h2>Ciao sono la pagina di dettaglio del post {post.titolo}</h2>
+            <p>{post.tags}</p>
+        </>
+    )
 
 }
